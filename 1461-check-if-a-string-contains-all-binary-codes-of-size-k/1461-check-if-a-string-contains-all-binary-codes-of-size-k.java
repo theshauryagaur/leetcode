fced456129fe75fefd0b400e13b8aac34c529ec0
Shaurya Gaur <shauryagaur6@gmail.com>
2026-06-05 14:43:37 +0530

    1461-check-if-a-string-contains-all-binary-codes-of-size-k Hash Table

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.java
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.java
@@ -1,19 +1,16 @@
 class Solution {
-  public boolean hasAllCodes(String s, int k) {
-    int n = s.length();
-    int total = (int) Math.pow(2,k);
+    public boolean hasAllCodes(String s, int k) {
+        int n = s.length();
+        int total = 1<<k;;
 
-    if( (n-k+1) < total ) return false;
-
-    int codes = 1 << k; // 2 ^ k
-    HashSet<Integer> st = new HashSet<>();
-
-    for (int i = 0; i <= n - k; i++) {
-        st.add(Integer.parseInt(s.substring(i, i + k), 2));
-        if (st.size() == codes) {
-            return true;
+        if( (n-k+1) < total ) return false;
+        
+        int codes = (int) Math.pow(2, k);
+        Set<String> set = new HashSet<>();
+        for(int i = 0; i <= n - k; i++){
+            String str = s.substring(i, i+k);
+            set.add(str);
         }
+        return set.size() == codes;
     }
-    return st.size() == codes;
-  }
 }
