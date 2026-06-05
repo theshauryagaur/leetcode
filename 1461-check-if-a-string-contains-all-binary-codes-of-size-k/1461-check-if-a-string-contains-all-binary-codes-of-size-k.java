cc47716b11559ce5b3d80f3d77fa83356aedcfa0
Shaurya Gaur <shauryagaur6@gmail.com>
2026-06-05 14:39:48 +0530

    1461-check-if-a-string-contains-all-binary-codes-of-size-k Hash Table

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.java
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.java
@@ -1,62 +1,19 @@
 class Solution {
-    public class RollingHash{
-        private final long[] power;
-        private final long[] hash;
-        private final long base;
-        private final long mod;
-        private final int k;
-        
-        RollingHash(String s, long base, long mod, int k){
-            int n = s.length();
+  public boolean hasAllCodes(String s, int k) {
+    int n = s.length();
+    int total = (int) Math.pow(2,k);
 
-            this.base = base;
-            this.mod = mod;
-            this.k = k;
+    if( (n-k+1) < total ) return false;
 
-            power = new long[n+1];
-            hash = new long[n+1];
+    int codes = 1 << k; // 2 ^ k
+    HashSet<Integer> st = new HashSet<>();
 
-            power[0] = 1;
-            for(int i=0; i<n; i++){
-                power[i+1] = power[i] * base % mod;
-            }
-
-            hash[0] = 0;
-            for(int i=0; i<n; i++){
-                hash[i+1] = (hash[i] * base + s.charAt(i)) % mod;
-            }
-
-        }
-
-        long get(int l, int r){
-            long h = hash[r+1] - hash[l] * power[r-l+1] % mod;
-            return (h+mod)%mod;
-        }
-
-        long getK(int l){
-            long h = hash[l+k] - hash[l] * power[k] % mod;
-            return (h+mod)%mod;
+    for (int i = 0; i <= n - k; i++) {
+        st.add(Integer.parseInt(s.substring(i, i + k), 2));
+        if (st.size() == codes) {
+            return true;
         }
     }
-    
-    public boolean hasAllCodes(String s, int k) {
-        int n = s.length();
-        int total = (int) Math.pow(2,k);
-
-        if( (n-k+1) < total ) return false;
-
-        HashSet<Long> set = new HashSet<>();
-        int codes = 0;
-
-        RollingHash r = new RollingHash(s, 31, 1_000_000_007, k);
-        for(int i=0; i<n-k+1; i++){
-            long code = r.getK(i);
-            if( !set.contains(code) ){
-                codes++;
-                set.add(code);
-            }
-        }
-
-        return codes==total ? true : false;
-    }
+    return st.size() == codes;
+  }
 }
