ab24519e6fc974ef58ba7d2033cda134958a5153
Shaurya Gaur <shauryagaur6@gmail.com>
2026-06-06 13:43:22 +0530

    11 ms (91.85%) Hash Table .java

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
@@ -1,47 +1,17 @@
 class Solution {
     public int numberOfSubstrings(String s) {
+        
         int n = s.length();
+        int cnt = 0;
 
-        int[] a = new int[n];
-        int indA = -1;
-        int[] b = new int[n];
-        int indB = -1;
-        int[] c = new int[n];
-        int indC = -1;
-
-        for(int i = n-1; i>=0; i--){
-            char x = s.charAt(i);
-            if(x == 'a'){
-                indA = i;
-            }
-            else if(x == 'b'){ 
-                indB = i;
-            }
-            else if(x == 'c') {
-                indC = i;
-            }
-
-            a[i] = indA;
-            b[i] = indB;
-            c[i] = indC;
-        }
-
-        int ans = 0;
+        int[] ind = new int[3];
+        Arrays.fill(ind,-1);
+        
         for(int i=0; i<n; i++){
-            indA = a[i]; indB = b[i]; indC = c[i];
+            ind[s.charAt(i)-'a'] = i;
 
-            if(indA == -1 || indB==-1 || indC == -1) break;
-
-            if(s.charAt(i) == 'a'){
-                ans += n - Math.max(b[i],c[i]);
-            }
-            else if(s.charAt(i) == 'b'){
-                ans += n - Math.max(a[i],c[i]);
-            }
-            else if(s.charAt(i) == 'c'){
-                ans += n - Math.max(b[i],a[i]);
-            }
+            cnt += Math.min(ind[0],Math.min(ind[1], ind[2])) + 1;
         }
-        return ans;
+        return cnt;
     }
 }
