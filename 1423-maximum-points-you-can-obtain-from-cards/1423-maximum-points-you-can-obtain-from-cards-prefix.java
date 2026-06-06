f6f7facaa72b907094cd0ebb578fee2e85b53304
Shaurya Gaur <shauryagaur6@gmail.com>
2026-06-06 15:48:55 +0530

    1423-maximum-points-you-can-obtain-from-cards-prefix.java

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards-prefix.java
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards-prefix.java
@@ -1,24 +1,25 @@
 class Solution {
     public int maxScore(int[] pts, int k) {
         int n = pts.length;
-        
+        int[] prefix = new int[n+1];
+
+        prefix[0] = 0;
+        for(int i=0; i<n; i++){
+            prefix[i+1] = prefix[i] + pts[i];
+        }
+
+        int total = prefix[n];
+
+        if(k == n) return total;
+
         int remCards = n-k;
-        int sumRemCards = 0;
-        for(int i=0; i<remCards; i++){
-            sumRemCards += pts[i];
+        int sumRemCards = Integer.MAX_VALUE;
+
+        for(int i=0; i < n-remCards+1 ; i++){
+            sumRemCards = Math.min(sumRemCards, prefix[i+remCards] - prefix[i]);
         }
 
-        int total = sumRemCards;
-
-        int minSumRemCards = sumRemCards;
-
-        for(int i=remCards; i < n ; i++){
-            total += pts[i];
-            sumRemCards += pts[i]-pts[i-remCards];
-            minSumRemCards = Math.min(minSumRemCards, sumRemCards);
-
-        }
-
-        return total - minSumRemCards;
+        return total - sumRemCards;
     }
+}
 }
