575495202d94c9d3aa10f1d7bf3e22c76af0ac9c
Shaurya Gaur <shauryagaur6@gmail.com>
2026-06-08 23:12:28 +0530

    4 ms (87.32%) Array .java

--- 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.java
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.java
@@ -4,13 +4,15 @@ class Solution {
 
         int total = 0;
         int[] ans = new int[n];
-        
+
         for(int i=0; i<n; i++){
             ans[i] = total;
             total += nums[i];
         }
         for(int i=0; i<n; i++){
             int x = ans[i];
+            //( total-summation(0,i-1) )- (n-i)nums[i] + i*nums[i] - summation(0,i-1);
+            // this gives |xj-xi| for j>=i    and       this gives |xj-xi| = (xi-xj) for j<i
             ans[i] = total - (n-2*i)*nums[i] - 3*x;
             
             total += nums[i];
