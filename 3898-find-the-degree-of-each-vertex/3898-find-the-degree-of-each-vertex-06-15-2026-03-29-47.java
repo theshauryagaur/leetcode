f050ecd82798e35b1cf11eb877dd905ee5eb3283
Shaurya Gaur <shauryagaur6@gmail.com>
2026-06-15 03:29:47 +0530

    2 ms (69.63%) Array .java

---
+++ 3898-find-the-degree-of-each-vertex/3898-find-the-degree-of-each-vertex-06-15-2026-03-29-47.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] findDegrees(int[][] mat) {
+        int[] deg = new int[mat.length];
+
+        for(int i=0; i<mat.length; i++){
+            for(int j=0; j<i; j++){
+                if(mat[i][j] == 1) {
+                    deg[i]++;
+                    deg[j]++;
+                }
+            }
+        }
+        return deg;
+    }
+}
