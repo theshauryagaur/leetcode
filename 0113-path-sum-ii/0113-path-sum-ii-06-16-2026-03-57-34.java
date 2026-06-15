17f7e21d4ad044547c1ccd4b65ca78ffa2c93716
Shaurya Gaur <shauryagaur6@gmail.com>
2026-06-16 03:57:35 +0530

    1 ms (99.99%) Backtracking .java

---
+++ 0113-path-sum-ii/0113-path-sum-ii-06-16-2026-03-57-34.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void helper(TreeNode node, int tar, int curr, List<List<Integer>> ans, List<Integer> list){
+        if(node == null){
+            return;
+        }
+        if(node.left == null && node.right == null){
+            curr += node.val;
+            list.add(node.val);
+            if(tar == curr){
+                ans.add(new ArrayList<>(list));
+            }
+            list.remove(list.size()-1);
+            return;
+        }
+
+        curr += node.val;
+
+        list.add(node.val);
+        helper(node.left, tar, curr, ans, list);
+
+        helper(node.right, tar, curr, ans, list);
+        list.remove(list.size()-1);
+        
+        return;
+    }
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> curr = new ArrayList<>();
+
+        helper(root, targetSum, 0, ans, curr);
+            
+        return ans;
+        
+        
+    }
+}
