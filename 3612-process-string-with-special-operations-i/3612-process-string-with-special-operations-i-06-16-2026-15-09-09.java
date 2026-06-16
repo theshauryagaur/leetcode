26f1592d63366c1a3d29e563c5b6500fad4b79f7
Shaurya Gaur <shauryagaur6@gmail.com>
2026-06-16 15:09:10 +0530

    4 ms (76.47%) String .java

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i-06-16-2026-15-09-09.java
@@ -0,0 +1,30 @@
+class Solution {
+    public void reverse(StringBuilder sb){
+        int len = sb.length();
+        for(int i=0; i<len/2; i++){
+            char temp = sb.charAt(i);
+            sb.setCharAt(i, sb.charAt(len-i-1));
+            sb.setCharAt(len-i-1, temp);
+        }
+    }
+    public String processStr(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+            if(c == '*'){
+                if(sb.length() != 0) sb.deleteCharAt(sb.length()-1);
+            }
+            else if(c == '#'){
+                sb.append(sb);
+            }
+            else if(c == '%'){
+                reverse(sb);
+            }
+            else{
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
