67ea9d3280f1fb1a2b9a35e0436c35611a755e84
Shaurya Gaur <shauryagaur6@gmail.com>
2026-06-25 01:48:00 +0530

    23 ms (93.95%) Array .java

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum-06-25-2026-01-47-59.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        int n = nums.length;
+        HashMap<Integer,Integer> preMod = new HashMap<>();
+        preMod.put(0,-1);
+        int rem = 0;
+        for(int i=0; i<n; i++){
+            rem = (rem+nums[i])%k;
+
+            if(preMod.containsKey(rem)){
+                if(i - preMod.get(rem) >= 2) return true;
+            }
+            else preMod.put(rem,i); 
+        }
+        return false;
+    }
+}
