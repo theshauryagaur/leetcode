d625844f284a828c306bd69a4310d7d6219cc760
Shaurya Gaur <shauryagaur6@gmail.com>
2026-06-25 22:24:13 +0530

    19 ms (41.02%) Array .java

---
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets-06-25-2026-22-24-12.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean possible(int[] bloom, int day, int m, int k){
+        int bou = 0;
+        int cnt = 0;
+        for(int i=0; i<bloom.length; i++){
+            if(bloom[i] <= day){
+                cnt++;
+            }
+            else {
+                cnt = 0;
+            }
+            if(cnt == k){
+                bou++;
+                cnt = 0;
+            }
+
+            if(bou >= m) return true;
+        }
+        return bou>=m;
+    }
+    public int minDays(int[] bloomDay, int m, int k) {
+        int n = bloomDay.length;
+        if((long)m*k > n) return -1;
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for(int i: bloomDay){
+            min = Math.min(min, i);
+            max = Math.max(max, i);
+        }
+
+        int l=min, r=max;
+        while(l<=r){
+            int mid = l + (r-l)/2;
+
+            if(possible(bloomDay, mid, m, k)==true){
+                r = mid-1;
+            }
+            else l = mid+1;
+
+        }
+        return l;
+    }
+}
