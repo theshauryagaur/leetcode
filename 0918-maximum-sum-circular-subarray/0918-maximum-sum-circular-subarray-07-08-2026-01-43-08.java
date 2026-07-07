4faa1ac498b197b140896120e81c8f15a76bd570
Shaurya Gaur <shauryagaur6@gmail.com>
2026-07-08 01:43:10 +0530

    52 ms (6.53%) Array .java

---
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray-07-08-2026-01-43-08.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int maxSubarraySumCircular(int[] nums) {
+        int n = nums.length;
+        int max = Integer.MIN_VALUE;
+        int currSum = 0;
+        int prefixMax = Integer.MIN_VALUE;
+        int preSum = 0;
+        int suffixMax = Integer.MIN_VALUE;
+        int sufSum = 0;
+
+        int[] pre = new int[n];
+        int[] suf = new int[n];
+
+        for(int i=0; i<n; i++){
+            currSum += nums[i];
+            preSum += nums[i];
+            sufSum += nums[n-i-1];
+
+            max = Math.max(max, currSum);
+            
+            if(currSum <= 0) currSum = 0;
+            
+            prefixMax = Math.max(prefixMax, preSum);
+            suffixMax = Math.max(suffixMax, sufSum);
+
+            pre[i] = prefixMax;
+            suf[n-i-1] = suffixMax;
+        }
+        System.out.print(max);
+        System.out.print(Arrays.toString(pre));
+        System.out.print(Arrays.toString(suf));
+        int ans = Integer.MIN_VALUE;
+        for(int i=0; i<n-1; i++){
+            ans = Math.max(ans, pre[i]+suf[i+1]);
+        }
+
+        return Math.max(ans, max);
+    }
+}
