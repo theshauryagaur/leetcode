e521f45ade28e55e7c8f090ef212300930b311bb
Shaurya Gaur <shauryagaur6@gmail.com>
2026-08-02 00:41:09 +0530

    2 ms (37.4%) Array .java

---
+++ 0486-predict-the-winner/0486-predict-the-winner-08-02-2026-00-41-08.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean help(int[] nums, int l, int r, int p1, int p2, int turn){
+        if(r<l) return p1>=p2 ? true:false;
+
+        if(turn == 0){
+            return help(nums, l+1, r, p1+nums[l], p2, turn^1) || help(nums, l, r-1, p1+nums[r], p2, turn^1);
+        }
+        else{
+            return help(nums, l+1, r, p1, p2+nums[l], turn^1) && help(nums, l, r-1, p1, p2+nums[r], turn^1);
+        }
+    }
+    public boolean predictTheWinner(int[] nums) {
+        int n = nums.length;
+        return help(nums, 0, n-1, 0, 0, 0);
+    }
+}
