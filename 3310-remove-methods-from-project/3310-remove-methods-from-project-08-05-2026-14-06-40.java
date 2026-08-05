fd610cc91834aea2ce78e92ab40e4b282c56dd0b
Shaurya Gaur <shauryagaur6@gmail.com>
2026-08-05 14:06:42 +0530

    61 ms (81.73%) Depth-First Search .java

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project-08-05-2026-14-06-40.java
@@ -0,0 +1,52 @@
+class Solution {
+    public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
+        List<Integer>[] adj = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            adj[i] = new ArrayList<>();
+        }
+
+        for(int[] invo : invocations){
+            adj[invo[0]].add(invo[1]);
+        }
+
+        boolean[] visi = new boolean[n];
+        visi[k] = true;
+
+        Queue<Integer> q = new ArrayDeque<>();
+        q.offer(k);
+        while(!q.isEmpty()){
+            int curr = q.poll();
+            for(int v: adj[curr]){
+                if(!visi[v]){
+                    visi[v] = true;
+                    q.offer(v);
+                }
+            }
+        }
+
+        boolean canRemove = true;
+        for(int i=0; i<n; i++){
+            if(!visi[i]){
+                for(int v: adj[i]){
+                    if(visi[v]){
+                        canRemove = false;
+                        break;
+                    }
+                }
+            }
+        }
+
+        List<Integer> ans = new ArrayList<>();
+        if(canRemove){
+            for(int i=0; i<n; i++){
+                if(!visi[i]) ans.add(i);
+            }
+        }
+        else{
+            for(int i=0; i<n; i++){
+                ans.add(i);
+            }
+        }
+        return ans;
+    }
+}
