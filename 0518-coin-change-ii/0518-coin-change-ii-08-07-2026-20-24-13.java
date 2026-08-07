13c0b112e363b031457f9605d898c44e47f61054
Shaurya Gaur <shauryagaur6@gmail.com>
2026-08-07 20:24:13 +0530

    6 ms (75.24%) Array .java

---
+++ 0518-coin-change-ii/0518-coin-change-ii-08-07-2026-20-24-13.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        int n = coins.length;
+
+        if(amount == 0) return 1;
+
+        int m = amount;
+        int[] dp = new int[m+1];
+
+        dp[0] = 1;
+
+        for(int coin: coins){
+            for(int j=coin; j<=m; j++){
+                if(j - coin < 0) dp[j] = 0;
+                else dp[j] = dp[j] + dp[j - coin];
+            }
+        }
+
+        return dp[m];
+    }
+}
