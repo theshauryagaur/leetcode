35d36debc30a60537bbe954b21b6322972071747
Shaurya Gaur <shauryagaur6@gmail.com>
2026-08-22 20:07:51 +0530

    139 ms (35.59%) Array .java

---
+++ 3418-maximum-amount-of-money-robot-can-earn/3418-maximum-amount-of-money-robot-can-earn-08-22-2026-20-07-50.java
@@ -0,0 +1,42 @@
+class Solution {
+    int[][][] dp;
+    int m;
+    int n;
+
+    public int help(int[][] coin, int i, int j, int k){
+        if(i < 0 || i >= m || j < 0 || j >= n) return -2000000000;
+
+        if(i == m-1 && j == n-1){
+            if(coin[i][j] >= 0){
+                return coin[i][j];
+            }
+            else{
+                if(k > 0) return 0;
+                else return coin[i][j];
+            }
+        }
+ 
+        if(dp[i][j][k] != Integer.MIN_VALUE) return dp[i][j][k];
+
+        int protect = Integer.MIN_VALUE;
+        int take = Math.max(help(coin, i+1, j, k), help(coin, i, j+1, k)) + coin[i][j];
+        if(k > 0){
+            protect = Math.max(help(coin, i+1, j, k-1), help(coin, i, j+1, k-1));
+        }
+
+        return dp[i][j][k] = Math.max(take, protect);
+    }
+    public int maximumAmount(int[][] coins) {
+        m = coins.length;
+        n = coins[0].length;
+
+        dp = new int[m][n][3];
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                Arrays.fill(dp[i][j], Integer.MIN_VALUE);
+            }
+        }
+        
+        return help(coins, 0, 0, 2);
+    }
+}
