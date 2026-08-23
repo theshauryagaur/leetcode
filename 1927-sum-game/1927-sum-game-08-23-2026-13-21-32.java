5cc5a641c3ce0abf95c46cef24c9679237aaeafa
Shaurya Gaur <shauryagaur6@gmail.com>
2026-08-23 13:21:33 +0530

    6 ms (97.08%) Math .java

---
+++ 1927-sum-game/1927-sum-game-08-23-2026-13-21-32.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean sumGame(String num) {
+        int n=num.length();
+        int left=0, right=0;
+        int lSum=0, rSum=0;
+
+        for(int i=0; i<n/2; i++){
+            char c = num.charAt(i);
+            if(c == '?') left++;
+            else lSum += c-'0';
+        }
+        for(int i=n/2; i<n; i++){
+            char c = num.charAt(i);
+            if(c == '?') right++;
+            else rSum += c-'0';
+        }
+
+        if(lSum < rSum){
+            lSum ^= rSum;
+            rSum ^= lSum;
+            lSum ^= rSum;
+
+            left ^= right;
+            right ^= left;
+            left ^= right;
+        }
+
+        int total = left + right;
+        if(total == 0){
+            if(lSum == rSum) return false;
+            return true;
+        }
+        if(total%2 == 1) return true;
+        else{
+            if((lSum-rSum)*2 == (right-left)*9) return false;
+        }
+        return true;
+    }
+}
