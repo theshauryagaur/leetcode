2d2daff83d0c8c184246a4858438aac029ffed71
Shaurya Gaur <shauryagaur6@gmail.com>
2026-09-04 02:55:07 +0530

    8 ms (26.06%) Array .java

---
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum-09-04-2026-02-55-05.java
@@ -0,0 +1,26 @@
+class Solution { 
+    public int minFallingPathSum(int[][] matrix) {
+        int n = matrix.length;
+
+        int[][] dp = new int[n][n];
+
+        dp[n-1] = matrix[n-1];
+
+        for(int i=n-2; i>=0; i--){
+            for(int j=0; j<n; j++){
+                int left = j-1 >= 0 ? dp[i+1][j-1] : 1000000;
+                int down = dp[i+1][j];
+                int right = j+1 < n ? dp[i+1][j+1] : 1000000;
+
+                dp[i][j] = matrix[i][j] + Math.min(left, Math.min(down, right));
+            }
+        }
+
+        int ans = 1000000;
+        for(int j=0; j<n; j++){
+            ans = Math.min(ans, dp[0][j]);
+        }
+
+        return ans;
+    }
+}
