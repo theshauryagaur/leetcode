3d9e49c0128918be488ae39d48b7bc767af1f493
Shaurya Gaur <shauryagaur6@gmail.com>
2026-09-07 23:29:19 +0530

    1 ms (76.79%) Array .java

---
+++ 0890-find-and-replace-pattern/0890-find-and-replace-pattern-09-07-2026-23-29-18.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<String> findAndReplacePattern(String[] words, String pattern) {
+        List<String> ans = new ArrayList<>();
+        int n = pattern.length();
+
+        int[] patt = new int[26];
+        for(int i=0; i<n; i++) patt[i] = pattern.charAt(i)-'a';
+        
+        for(String s: words){
+            int[] map = new int[26];
+            int[] mapped = new int[26];
+
+            boolean canbe = true;
+
+            for(int i=0; i<n; i++){
+                int ch = patt[i]; //pattern char
+
+                int wordChar = s.charAt(i)-'a';
+
+                if((map[ch] == 0 && mapped[wordChar] == 0) || (map[ch]-1 == s.charAt(i)-'a')){ // either both word and patt char are not mapped before or they are mapped to eachother previously
+                    mapped[wordChar] = 1;
+                    map[ch] = wordChar+1;
+                } 
+                else {
+                    canbe = false;
+                    break;
+                } 
+            }
+            if(canbe) ans.add(s);
+        }
+        return ans;
+    }
+}
